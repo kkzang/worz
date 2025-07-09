0ce253f0a8919646418e4daeb03b262af196182b
kkzang <likeagogh@gmail.com>
2025-07-10 03:11:36 +0900

    Updated index.tsx

--- app/(tabs)/explore/index.tsx
+++ app/(tabs)/explore/index.tsx
@@ -334,7 +334,7 @@ const styles = StyleSheet.create({
     color: Colors.textSecondary,
   },
   adBanner: {
-    height: 180,
+    height: 90,
     backgroundColor: Colors.backgroundDark,
     justifyContent: 'center',
     alignItems: 'center',
